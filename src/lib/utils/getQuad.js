a6bd8126b48dce7ba7d1b61f8ff7fa1ee21f0d28
anvaka <anvaka@gmail.com>
2016-08-14 00:17:20 -0700

    Properly read binary groups

--- src/lib/utils/getQuad.js
+++ src/lib/utils/getQuad.js
@@ -21,14 +21,23 @@ function parseQuad(buffer, quadInfo, tree) {
   if (!quadInfo.isTerminal) {
     points = readPoints(src, 0, src.length, quadInfo.name)
   } else {
-    var indexLength = src[0];
+    var indexLength = (src[0])/4;
+    // first int32 is encoded quad name, second - offset in bytes to its buffer.
     var found = false;
-    for (var i = 1; i < indexLength + 1; i += 2) {
-      var quadName = quadNameFromBinary(src[i])
+    // iterate over index part of the array
+    for (var i = 0; i < indexLength; i += 2) {
+      var recordOffset = 1 + i; // 1 - size of the index.
+      var quadName = quadNameFromBinary(src[recordOffset])
       if (quadName === quadInfo.name) {
-        var offset = src[i + 1]
-        var byteLength = (i + 3) < src.length ? src[i + 3] - offset : src.length - offset;
-        var arrayLength = byteLength / 4;
+        var offset = src[recordOffset + 1];
+        var arrayLength;
+
+        if (recordOffset + 3 < indexLength) {
+          var byteLength = src[recordOffset + 3] - offset
+          arrayLength = byteLength / 4;
+        } else {
+          arrayLength = src.length - offset / 4;
+        }
         points = readPoints(src, offset / 4, arrayLength, quadInfo.name);
         found = true;
         break;
